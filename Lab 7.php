eea3189336ea69809cd55ca1420d0a6d79e3d2f6
Rishabh Singh <irishabhsingh10@gmail.com>
2019-11-22 22:02:26 +0530

    Update Lab 7.php

--- Lab 7.php
+++ Lab 7.php
@@ -13,8 +13,10 @@ transform: translate(-50%,-50%);
 }
 body{background-color:black;}
 </style>
+</head>
+<body>
 <p> <?php 
 date_default_timezone_set("Asia/Calcutta");
 echo date(" h: i : s A");?> </p>
-</head>
+</body>
 </html>
